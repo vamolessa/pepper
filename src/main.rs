ab8e017673a8272e7bbb6743d62514adc654aace
Matheus Lessa <matheus@fastmail.com>
2020-04-16 21:04:23 -0300

    tentando algo mais simples

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,12 @@ use std::io::{stdout, Write};
 
 use crossterm::{
     cursor, handle_command,
-    style::{Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    terminal::{self, Clear, ClearType},
+    style::{Print, ResetColor},
+    terminal::{Clear, ClearType},
     Result,
 };
 
-use ropey;
-use syntect::{
-    highlighting::{self, HighlightIterator, HighlightState, Highlighter, Theme},
-    parsing::{self, ParseState, ScopeStack, SyntaxReference},
-};
-
 mod buffer;
-
-fn to_crossterm_color(c: highlighting::Color) -> crossterm::style::Color {
-    crossterm::style::Color::Rgb {
-        r: c.r,
-        g: c.g,
-        b: c.b,
-    }
-}
-
-pub struct HighlightLines<'a> {
-    pub highlighter: Highlighter<'a>,
-    pub parse_state: ParseState,
-    pub highlight_state: HighlightState,
-}
-
-impl<'a> HighlightLines<'a> {
-    pub fn new(syntax: &SyntaxReference, theme: &'a Theme) -> Self {
-        let highlighter = Highlighter::new(theme);
-        let highlight_state = HighlightState::new(&highlighter, ScopeStack::new());
-        HighlightLines {
-            highlighter,
-            parse_state: ParseState::new(syntax),
-            highlight_state,
-        }
-    }
-}
 
 fn main() -> Result<()> {
     crossterm::terminal::enable_raw_mode()?;
@@ -48,45 +16,14 @@ fn main() -> Result<()> {
     let mut stdout = stdout.lock();
 
     let src = include_str!("main.rs");
-    let rope = ropey::Rope::from_str(src);
 
-    let syntax_set = parsing::SyntaxSet::load_defaults_newlines();
-    let theme_set = highlighting::ThemeSet::load_defaults();
-    let syntax = syntax_set.find_syntax_by_extension("rs").unwrap();
-
-    let mut h = HighlightLines::new(&syntax, &theme_set.themes["base16-mocha.dark"]);
-
-    let mut highlighted_lines = Vec::new();
-    for line in rope.lines() {
-        use std::fmt::Write;
-        let line: String = line.chars().collect();
-        let mut highlighted_line = String::new();
-
-        let ops = h.parse_state.parse_line(&line[..], &syntax_set);
-        for (style, slice) in
-            HighlightIterator::new(&mut h.highlight_state, &ops[..], &line[..], &h.highlighter)
-        {
-            handle_command!(
-                highlighted_line,
-                SetForegroundColor(to_crossterm_color(style.foreground))
-            )?;
-            handle_command!(
-                highlighted_line,
-                SetBackgroundColor(to_crossterm_color(style.background))
-            )?;
-            handle_command!(highlighted_line, Print(slice))?;
-        }
-        highlighted_lines.push(highlighted_line);
-    }
-
-    for line in &highlighted_lines {
+    for line in src.lines() {
         handle_command!(stdout, Print(line))?;
         handle_command!(stdout, Clear(ClearType::UntilNewLine))?;
-        // handle_command!(stdout, cursor::MoveToNextLine(1))?;
+        handle_command!(stdout, cursor::MoveToNextLine(1))?;
     }
 
     handle_command!(stdout, ResetColor)?;
-    handle_command!(stdout, cursor::MoveToNextLine(1))?;
     stdout.flush()?;
     crossterm::terminal::disable_raw_mode()?;
     Ok(())
