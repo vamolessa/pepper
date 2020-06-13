41aa6f1f05a641d7b982bcb63e9ad0812b34a760
Matheus Lessa <matheus@fastmail.com>
2020-06-13 17:49:55 -0300

    um pouco mais de undo

--- src/undo.rs
+++ src/undo.rs
@@ -5,43 +5,52 @@ pub enum Text {
     String(String),
 }
 
-pub enum Command {
-    Insert(Text, Vec<BufferPosition>),
-    Delete(Vec<(BufferPosition, BufferPosition)>),
+pub enum Edit {
+    Insert(Text, BufferPosition),
+    Delete(BufferPosition, BufferPosition),
 }
 
 #[derive(Default)]
 pub struct Undo {
-    history: Vec<Command>,
+    history: Vec<(Edit, usize)>,
     current_history_index: usize,
+    current_edit_group_id: usize,
 }
 
 impl Undo {
-    pub fn push_command(&mut self, command: Command) {
+    pub fn push_edit(&mut self, edit: Edit) {
         self.current_history_index += 1;
         self.history.truncate(self.current_history_index);
-        self.history.push(command);
+        self.history.push((edit, self.current_edit_group_id));
     }
 
-    pub fn undo(&mut self) -> Option<&Command> {
+    pub fn commit_edits(&mut self) {
+        self.current_edit_group_id += 1;
+    }
+
+    pub fn undo(&mut self) -> Option<&Edit> {
+        self.commit_edits();
+
         if self.current_history_index > 0 {
             self.current_history_index -= 1;
         }
 
         if self.current_history_index < self.history.len() {
-            Some(&self.history[self.current_history_index])
+            Some(&self.history[self.current_history_index].0)
         } else {
             None
         }
     }
 
-    pub fn redo(&mut self) -> Option<&Command> {
+    pub fn redo(&mut self) -> Option<&Edit> {
+        self.commit_edits();
+
         if self.current_history_index < self.history.len() - 1 {
             self.current_history_index += 1;
         }
 
         if self.current_history_index < self.history.len() {
-            Some(&self.history[self.current_history_index])
+            Some(&self.history[self.current_history_index].0)
         } else {
             None
         }
