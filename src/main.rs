dfa0f2eadcdc9a117078311fc3ea281796f01bff
Matheus Lessa <matheus@fastmail.com>
2020-08-26 18:35:15 -0300

    vamo assim mesmo

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,35 @@ mod theme;
 mod tui;
 
 fn main() {
+    let mut client_target_map = editor::ClientTargetMap::default();
+    let mut operations = editor_operation::EditorOperationSerializer::default();
+    let config = config::Config::default();
+    let mut keymaps = keymap::KeyMapCollection::default();
+    let mut buffers = buffer::BufferCollection::default();
+    let mut buffer_views = buffer_view::BufferViewCollection::default();
+    let mut current_buffer_view_handle = None;
+
+    let context = script::ScriptContext {
+        target_client: connection::TargetClient::All,
+        client_target_map: &mut client_target_map,
+        operations: &mut operations,
+
+        config: &config,
+        keymaps: &mut keymaps,
+        buffers: &mut buffers,
+        buffer_views: &mut buffer_views,
+        current_buffer_view_handle: &mut current_buffer_view_handle,
+    };
+
+    let mut scripts = script::ScriptEngine::new();
+    scripts
+        .eval(
+            context,
+            "my_print(\"sadsad\")"
+        )
+        .unwrap();
+    return;
+
     if let Err(e) = application::run() {
         eprintln!("{}", e);
     }
--- src/script.rs
+++ src/script.rs
@@ -1,44 +1,77 @@
 use std::path::PathBuf;
 
-use rhai::{module_resolvers::FileModuleResolver, Engine, EvalAltResult, Scope, AST};
+use rhai::{
+    def_package,
+    module_resolvers::FileModuleResolver,
+    packages::{EvalPackage, Package},
+    Engine, EvalAltResult, ImmutableString, Scope,
+};
+
+use crate::{
+    buffer::BufferCollection,
+    buffer_view::{BufferViewCollection, BufferViewHandle},
+    config::Config,
+    connection::TargetClient,
+    editor::ClientTargetMap,
+    editor_operation::EditorOperationSerializer,
+    keymap::KeyMapCollection,
+};
+
+pub struct ScriptContext<'a> {
+    pub target_client: TargetClient,
+    pub client_target_map: &'a mut ClientTargetMap,
+    pub operations: &'a mut EditorOperationSerializer,
+
+    pub config: &'a Config,
+    pub keymaps: &'a mut KeyMapCollection,
+    pub buffers: &'a mut BufferCollection,
+    pub buffer_views: &'a mut BufferViewCollection,
+    pub current_buffer_view_handle: &'a mut Option<BufferViewHandle>,
+}
 
 pub struct ScriptEngine {
     engine: Engine,
-    entry_ast: Option<AST>,
 }
 
 impl ScriptEngine {
     pub fn new() -> Self {
-        let engine = Engine::new();
-        Self {
-            engine,
-            entry_ast: None,
-        }
+        let mut engine = Engine::new();
+        engine.load_package(EvalPackage::new().get());
+        engine.load_package(ApiPackage::new().get());
+
+        Self { engine }
     }
 
-    pub fn eval(&mut self, expression: &str) -> Result<i64, Box<EvalAltResult>> {
+    pub fn eval(
+        &mut self,
+        ctx: ScriptContext,
+        expression: &str,
+    ) -> Result<(), Box<EvalAltResult>> {
         let mut scope = Scope::new();
-
-        let ast = self.engine.compile_expression_with_scope(&scope, expression)?;
-        let ast = match &self.entry_ast {
-            Some(entry_ast) => entry_ast.merge(&ast),
-            None => ast,
-        };
-
-        self.engine.eval_ast_with_scope(&mut scope, &ast)
+        self.engine
+            .eval_expression_with_scope(&mut scope, expression)
     }
 
     pub fn load_entry_file(&mut self, path: PathBuf) -> Result<(), Box<EvalAltResult>> {
         let mut root_path = path.clone();
         root_path.pop();
+
         self.engine
             .set_module_resolver(Some(FileModuleResolver::new_with_path(root_path)));
 
         let mut scope = Scope::new();
         let ast = self.engine.compile_file_with_scope(&scope, path)?;
         self.engine.consume_ast_with_scope(&mut scope, &ast)?;
-        self.entry_ast = Some(ast);
 
+        self.engine
+            .set_module_resolver(Option::<FileModuleResolver>::None);
         Ok(())
     }
 }
+
+def_package!(rhai:ApiPackage:"pepper api", module, {
+    module.set_fn_1("my_print", |s: ImmutableString| {
+        println!("hello {}", s);
+        Ok(())
+    });
+});
