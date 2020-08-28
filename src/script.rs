73f521ffcdb161cd4306001a2f5bc4d7281b801e
Matheus Lessa <matheus@fastmail.com>
2020-08-28 10:05:57 -0300

    vamo de registry

--- src/script.rs
+++ src/script.rs
@@ -1,12 +1,6 @@
-use std::{
-    fs::File,
-    io::Read,
-    ops::{Deref, DerefMut},
-    path::Path,
-    sync::Arc,
-};
+use std::{fs::File, io::Read, path::Path, sync::Arc};
 
-use mlua::prelude::{Lua, LuaError, LuaResult, LuaTable, LuaUserData};
+use mlua::prelude::{FromLua, Lua, LuaError, LuaLightUserData, LuaRegistryKey, LuaResult, ToLua};
 
 use crate::{
     buffer::BufferCollection,
@@ -30,27 +24,6 @@ pub struct ScriptContext<'a> {
     pub current_buffer_view_handle: &'a mut Option<BufferViewHandle>,
 }
 
-#[derive(Clone)]
-pub struct ScriptContextRef(*mut ScriptContextRef);
-impl ScriptContextRef {
-    fn new(ctx: &mut ScriptContext) -> Self {
-        Self(ctx as *mut ScriptContext as *mut _)
-    }
-}
-impl Deref for ScriptContextRef {
-    type Target = ScriptContext<'static>;
-
-    fn deref(&self) -> &Self::Target {
-        unsafe { &*(self.0 as *const _) }
-    }
-}
-impl DerefMut for ScriptContextRef {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { &mut *(self.0 as *mut _) }
-    }
-}
-impl LuaUserData for ScriptContextRef {}
-
 pub struct ScriptEngine {
     lua: Lua,
 }
@@ -70,24 +43,28 @@ impl ScriptEngine {
         Ok(Self { lua })
     }
 
-    pub fn register_all_functions<'lua, F>(&'lua self, name: &str, func: F) -> LuaResult<()>
+    pub fn register_ctx_function<'lua, A, R>(
+        &'lua self,
+        name: &str,
+        func: fn(&mut ScriptContext, A) -> R,
+    ) -> LuaResult<()>
     where
-        F: FnOnce(&LuaTable<'lua>),
+        A: 'static + FromLua<'lua>,
+        R: 'static + ToLua<'lua>,
     {
-        let functions_table = self.lua.create_table()?;
-        func(&functions_table);
-        self.lua.globals().set(name, functions_table)?;
+        let func = self.lua.create_function(move |lua, args| {
+            let ctx: LuaLightUserData = lua.named_registry_value("ctx")?;
+            let ctx = unsafe { &mut *(ctx.0 as *mut _) };
+            Ok(func(ctx, args))
+        })?;
+        self.lua.globals().set(name, func)?;
         Ok(())
     }
 
     pub fn eval(&mut self, mut ctx: ScriptContext, source: &str) -> LuaResult<()> {
-        self.lua.scope(|scope| {
-            let ctx = scope.create_userdata(ScriptContextRef::new(&mut ctx))?;
-            self.lua.globals().set("ctx", ctx)?;
-
-            self.lua.load(source).exec()?;
-            Ok(())
-        })
+        self.update_ctx(&mut ctx)?;
+        self.lua.load(source).exec()?;
+        Ok(())
     }
 
     pub fn load_entry_file(&mut self, mut ctx: ScriptContext, path: &Path) -> LuaResult<()> {
@@ -99,19 +76,21 @@ impl ScriptEngine {
         file.read_to_string(&mut source)
             .map_err(|e| LuaError::ExternalError(Arc::new(e)))?;
 
-        self.lua.scope(|scope| {
-            let ctx = scope.create_userdata(ScriptContextRef::new(&mut ctx))?;
-            self.lua.globals().set("ctx", ctx)?;
+        self.update_ctx(&mut ctx)?;
 
-            let chunk = self.lua.load(&source);
-            let chunk = if let Some(name) = path.to_str() {
-                chunk.set_name(name)?
-            } else {
-                chunk
-            };
+        let chunk = self.lua.load(&source);
+        let chunk = if let Some(name) = path.to_str() {
+            chunk.set_name(name)?
+        } else {
+            chunk
+        };
 
-            chunk.exec()?;
-            Ok(())
-        })
+        chunk.exec()?;
+        Ok(())
+    }
+
+    fn update_ctx(&mut self, ctx: &mut ScriptContext) -> LuaResult<()> {
+        self.lua
+            .set_named_registry_value("ctx", LuaLightUserData(ctx as *mut ScriptContext as _))
     }
 }
