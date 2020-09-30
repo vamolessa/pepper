d28db0157e375ad393c125a61e2eb50bcce8b8c9
Matheus Lessa <matheus@fastmail.com>
2020-09-30 12:06:09 -0300

    da pra saber o tamanho do display de uma key

--- src/client_event.rs
+++ src/client_event.rs
@@ -165,87 +165,33 @@ pub enum Key {
     Esc,
 }
 
-impl<'de> Serialize<'de> for Key {
-    fn serialize<S>(&self, serializer: &mut S)
-    where
-        S: Serializer,
-    {
-        match self {
-            Key::None => 0u8.serialize(serializer),
-            Key::Backspace => 1u8.serialize(serializer),
-            Key::Enter => 2u8.serialize(serializer),
-            Key::Left => 3u8.serialize(serializer),
-            Key::Right => 4u8.serialize(serializer),
-            Key::Up => 5u8.serialize(serializer),
-            Key::Down => 6u8.serialize(serializer),
-            Key::Home => 7u8.serialize(serializer),
-            Key::End => 8u8.serialize(serializer),
-            Key::PageUp => 9u8.serialize(serializer),
-            Key::PageDown => 10u8.serialize(serializer),
-            Key::Tab => 11u8.serialize(serializer),
-            Key::Delete => 12u8.serialize(serializer),
-            Key::F(n) => {
-                13u8.serialize(serializer);
-                n.serialize(serializer);
-            }
-            Key::Char(c) => {
-                14u8.serialize(serializer);
-                c.serialize(serializer);
-            }
-            Key::Ctrl(c) => {
-                15u8.serialize(serializer);
-                c.serialize(serializer);
-            }
-            Key::Alt(c) => {
-                16u8.serialize(serializer);
-                c.serialize(serializer);
-            }
-            Key::Esc => 17u8.serialize(serializer),
-        }
-    }
-
-    fn deserialize<D>(deserializer: &mut D) -> Result<Self, DeserializeError>
-    where
-        D: Deserializer<'de>,
-    {
-        let discriminant = u8::deserialize(deserializer)?;
-        match discriminant {
-            0 => Ok(Key::None),
-            1 => Ok(Key::Backspace),
-            2 => Ok(Key::Enter),
-            3 => Ok(Key::Left),
-            4 => Ok(Key::Right),
-            5 => Ok(Key::Up),
-            6 => Ok(Key::Down),
-            7 => Ok(Key::Home),
-            8 => Ok(Key::End),
-            9 => Ok(Key::PageUp),
-            10 => Ok(Key::PageDown),
-            11 => Ok(Key::Tab),
-            12 => Ok(Key::Delete),
-            13 => {
-                let n = u8::deserialize(deserializer)?;
-                Ok(Key::F(n))
-            }
-            14 => {
-                let c = char::deserialize(deserializer)?;
-                Ok(Key::Char(c))
-            }
-            15 => {
-                let c = char::deserialize(deserializer)?;
-                Ok(Key::Ctrl(c))
-            }
-            16 => {
-                let c = char::deserialize(deserializer)?;
-                Ok(Key::Alt(c))
-            }
-            17 => Ok(Key::Esc),
-            _ => Err(DeserializeError),
-        }
-    }
-}
-
 impl Key {
+    pub const fn display_len(&self) -> usize {
+        match self {
+            Key::None => 0,
+            Key::Backspace => "<backspace>".len(),
+            Key::Enter => "<enter>".len(),
+            Key::Left => "<left>".len(),
+            Key::Right => "<right>".len(),
+            Key::Up => "<up>".len(),
+            Key::Down => "<down>".len(),
+            Key::Home => "<home>".len(),
+            Key::End => "<end>".len(),
+            Key::PageUp => "<pageup>".len(),
+            Key::PageDown => "<pagedown>".len(),
+            Key::Tab => "<tab>".len(),
+            Key::Delete => "<delete>".len(),
+            Key::F(n) => if *n >= 10 { "<f__>" } else { "<f_>" }.len(),
+            Key::Char(' ') => "<space>".len(),
+            Key::Char('<') => "<less>".len(),
+            Key::Char('>') => "<greater>".len(),
+            Key::Char(_) => "_".len(),
+            Key::Ctrl(_) => "<c-_>".len(),
+            Key::Alt(_) => "<a-_>".len(),
+            Key::Esc => "<esc>".len(),
+        }
+    }
+
     pub fn parse_all<'a>(raw: &'a str) -> KeyParser<'a> {
         KeyParser {
             len: raw.len(),
@@ -461,6 +407,86 @@ impl fmt::Display for Key {
     }
 }
 
+impl<'de> Serialize<'de> for Key {
+    fn serialize<S>(&self, serializer: &mut S)
+    where
+        S: Serializer,
+    {
+        match self {
+            Key::None => 0u8.serialize(serializer),
+            Key::Backspace => 1u8.serialize(serializer),
+            Key::Enter => 2u8.serialize(serializer),
+            Key::Left => 3u8.serialize(serializer),
+            Key::Right => 4u8.serialize(serializer),
+            Key::Up => 5u8.serialize(serializer),
+            Key::Down => 6u8.serialize(serializer),
+            Key::Home => 7u8.serialize(serializer),
+            Key::End => 8u8.serialize(serializer),
+            Key::PageUp => 9u8.serialize(serializer),
+            Key::PageDown => 10u8.serialize(serializer),
+            Key::Tab => 11u8.serialize(serializer),
+            Key::Delete => 12u8.serialize(serializer),
+            Key::F(n) => {
+                13u8.serialize(serializer);
+                n.serialize(serializer);
+            }
+            Key::Char(c) => {
+                14u8.serialize(serializer);
+                c.serialize(serializer);
+            }
+            Key::Ctrl(c) => {
+                15u8.serialize(serializer);
+                c.serialize(serializer);
+            }
+            Key::Alt(c) => {
+                16u8.serialize(serializer);
+                c.serialize(serializer);
+            }
+            Key::Esc => 17u8.serialize(serializer),
+        }
+    }
+
+    fn deserialize<D>(deserializer: &mut D) -> Result<Self, DeserializeError>
+    where
+        D: Deserializer<'de>,
+    {
+        let discriminant = u8::deserialize(deserializer)?;
+        match discriminant {
+            0 => Ok(Key::None),
+            1 => Ok(Key::Backspace),
+            2 => Ok(Key::Enter),
+            3 => Ok(Key::Left),
+            4 => Ok(Key::Right),
+            5 => Ok(Key::Up),
+            6 => Ok(Key::Down),
+            7 => Ok(Key::Home),
+            8 => Ok(Key::End),
+            9 => Ok(Key::PageUp),
+            10 => Ok(Key::PageDown),
+            11 => Ok(Key::Tab),
+            12 => Ok(Key::Delete),
+            13 => {
+                let n = u8::deserialize(deserializer)?;
+                Ok(Key::F(n))
+            }
+            14 => {
+                let c = char::deserialize(deserializer)?;
+                Ok(Key::Char(c))
+            }
+            15 => {
+                let c = char::deserialize(deserializer)?;
+                Ok(Key::Ctrl(c))
+            }
+            16 => {
+                let c = char::deserialize(deserializer)?;
+                Ok(Key::Alt(c))
+            }
+            17 => Ok(Key::Esc),
+            _ => Err(DeserializeError),
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct ClientEventSerializer(SerializationBuf);
 
@@ -563,6 +589,7 @@ mod tests {
         macro_rules! assert_key {
             ($key:expr) => {
                 let s = $key.to_string();
+                assert_eq!(s.len(), $key.display_len(), "wrong display len {}", &s);
                 assert_eq!($key, Key::parse(&mut s.chars()).unwrap());
             };
         }
