1a45e425e3eeaf13a514bc271f15c1c67ded1561
Matheus Lessa <matheus@fastmail.com>
2020-10-20 20:42:06 -0300

    opa, tava errado aquele detalhe

--- src/keymap.rs
+++ src/keymap.rs
@@ -109,9 +109,9 @@ impl Default for KeyMapCollection {
             this.parse_and_map(mode, "<c-m>", "<enter>").unwrap();
         }
 
-        this.parse_and_map(normal_mode, "<esc>", "<esc>cdcv/<esc>")
+        this.parse_and_map(normal_mode, "<esc>", "<esc>cdcV/<esc>")
             .unwrap();
-        this.parse_and_map(normal_mode, "<c-c>", "<esc>cdcv/<esc>")
+        this.parse_and_map(normal_mode, "<c-c>", "<esc>cdcV/<esc>")
             .unwrap();
 
         this.parse_and_map(normal_mode, "s", "/").unwrap();
