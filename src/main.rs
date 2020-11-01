efe0f43b7074de706e7470184284630c0db55a0b
Matheus Lessa <matheus@fastmail.com>
2020-10-31 21:07:07 -0300

    funciona!

--- src/main.rs
+++ src/main.rs
@@ -68,6 +68,7 @@ pub struct Args {
 }
 
 fn main() {
+    /*
     use ui::Ui;
     let stdout = std::io::stdout();
     let stdout = stdout.lock();
@@ -90,6 +91,7 @@ fn main() {
         }
     }
     return;
+    */
 
     let args: Args = argh::from_env();
     if args.version {
