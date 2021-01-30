04ab2b8b4835d919d424ddd89565cfe25c68a6be
Matheus Lessa <1696072+matheuslessarodrigues@users.noreply.github.com>
2021-01-30 13:50:23 -0300

    ainda não sei como abordar o paranauê :B

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,13 @@
-pub struct CommandCollection {
+pub struct CommandEngine {
     //
 }
+
+// picker-entries-from-process-lines "ls" | picker-pick
+
+// $client-index
+// $buffer-index
+// $buffer-path
+// $buffer-view-index
 
 // map-normal k ghla
 // map-read-line <c-k> <up>
@@ -50,6 +57,58 @@ function find_file()
 		if file then
 			buffer_open(file)
 		end
+	end)
+end
+
+function ripgrep()
+	read_line_prompt("rg:")
+	-- first a search pattern is read from the user
+	read_line_read(function(search_pattern)
+		-- early return if action was canceled
+		if search_pattern == nil then
+			return
+		end
+		
+		picker_reset()
+		read_line_prompt("jump(searching...):")
+		
+		local args = {"--line-number"}
+		local buffer_path = buffer_path()
+		-- maybe restrict searched files based on their extension
+		if buffer_path ~= nil then
+			local extension = string.match(buffer_path, "[^%.]%.(%w+)$")
+			if extension ~= nil then -- only search files with the same extension as current buffer
+				args[#args + 1] = "--type-add"
+				args[#args + 1] = "t:*." .. extension
+				args[#args + 1] = "-tt"
+			end
+		end
+		args[#args + 1] = search_pattern
+		
+		local picked = false
+		process_spawn("rg", args, nil, function(output)
+			-- this callback is called whenever there's new output from the spawned process
+			-- and once more at the end with 'output = nil' to indicate that the process finished
+			if picked or output == nil then
+				read_line_prompt("jump:")
+				return
+			end
+			-- iterate over 'output' lines
+			for match in string.gmatch(output, "[^\r\n]+") do
+				local file, line, text = string.match(match, "([^:]+):([^:]+):%s*(.*)")
+				picker_entry(file .. ":" .. line, text)
+			end
+		end)
+		
+		picker_pick(function(file_and_line)
+			picked = true
+			-- early return if no file was picked
+			if file_and_line == nil then
+				return
+			end
+			local file, line = string.match(file_and_line, "([^:]+):([^:]+)")
+			buffer_open(file, line)
+		end)
 	end)
 end
 */
