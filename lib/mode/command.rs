39f42dc88302cdd43e457e90f35c4ae9ac4f6ece
Matheus Lessa <1696072+matheuslessarodrigues@users.noreply.github.com>
2021-02-27 20:29:13 -0300

    vamo faze autocomplede

--- lib/mode/command.rs
+++ lib/mode/command.rs
@@ -6,14 +6,19 @@ use crate::{
     platform::Key,
 };
 
-#[derive(Default)]
-pub struct State {
-    history_index: usize,
+pub enum State {
+    NavigatingHistory(usize),
+    TypingCommand,
+}
+impl Default for State {
+    fn default() -> Self {
+        Self::TypingCommand
+    }
 }
 
 impl ModeState for State {
     fn on_enter(ctx: &mut ModeContext) {
-        ctx.editor.mode.command_state.history_index = ctx.editor.commands.history_len();
+        ctx.editor.mode.command_state = State::NavigatingHistory(ctx.editor.commands.history_len());
         ctx.editor.read_line.set_prompt(":");
         ctx.editor.read_line.set_input("");
     }
@@ -23,7 +28,7 @@ impl ModeState for State {
     }
 
     fn on_client_keys(ctx: &mut ModeContext, keys: &mut KeysIterator) -> Option<ModeOperation> {
-        let this = &mut ctx.editor.mode.command_state;
+        let state = &mut ctx.editor.mode.command_state;
         match ctx
             .editor
             .read_line
@@ -32,30 +37,34 @@ impl ModeState for State {
             ReadLinePoll::Pending => {
                 keys.put_back();
                 match keys.next(&ctx.editor.buffered_keys) {
-                    Key::Ctrl('n') | Key::Ctrl('j') => {
-                        if ctx.editor.picker.len() == 0 {
-                            this.history_index = ctx
+                    Key::Ctrl('n') | Key::Ctrl('j') => match state {
+                        State::NavigatingHistory(i) => {
+                            *i = ctx
                                 .editor
                                 .commands
                                 .history_len()
                                 .saturating_sub(1)
-                                .min(this.history_index + 1);
-                            let entry = ctx.editor.commands.history_entry(this.history_index);
+                                .min(*i + 1);
+                            let entry = ctx.editor.commands.history_entry(*i);
                             ctx.editor.read_line.set_input(entry);
-                        } else {
-                            // TODO: autocomplete
                         }
-                    }
-                    Key::Ctrl('p') | Key::Ctrl('k') => {
-                        if ctx.editor.picker.len() == 0 {
-                            this.history_index = this.history_index.saturating_sub(1);
-                            let entry = ctx.editor.commands.history_entry(this.history_index);
+                        State::TypingCommand => autocomplete(ctx),
+                    },
+                    Key::Ctrl('p') | Key::Ctrl('k') => match state {
+                        State::NavigatingHistory(i) => {
+                            *i = i.saturating_sub(1);
+                            let entry = ctx.editor.commands.history_entry(*i);
                             ctx.editor.read_line.set_input(entry);
-                        } else {
-                            // TODO: autocomplete
                         }
+                        State::TypingCommand => autocomplete(ctx),
+                    },
+                    _ => {
+                        *state = if ctx.editor.read_line.input().is_empty() {
+                            State::NavigatingHistory(ctx.editor.commands.history_len())
+                        } else {
+                            State::TypingCommand
+                        };
                     }
-                    _ => (),
                 }
             }
             ReadLinePoll::Canceled => Mode::change_to(ctx, ModeKind::default()),
@@ -113,4 +122,9 @@ impl ModeState for State {
 
         None
     }
+}
+
+fn autocomplete(ctx: &mut ModeContext) {
+    let input = ctx.editor.read_line.input();
+    //
 }
