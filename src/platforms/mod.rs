95972c6894f1708c5c223b30d54cc5c3a3fe6325
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2021-04-29 17:27:31 -0300

    vamo começar a fazer o port pra linux!

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -1,7 +1,13 @@
 #[cfg(windows)]
 mod windows;
 
+#[cfg(target_os = "linux")]
+mod linux;
+
 pub fn main() {
     #[cfg(windows)]
     windows::main();
+
+    #[cfg(target_os = "linux")]
+    linux::main();
 }
