9dbdd59edbac250e33aa13bb546453fa1690fb1e
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2021-05-02 13:09:02 -0300

    vamo começar o port pra bsd

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -4,10 +4,22 @@ mod windows;
 #[cfg(target_os = "linux")]
 mod linux;
 
+#[cfg(any(
+    target_os = "macos",
+    target_os = "freebsd",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "dragonfly",
+))
+mod bsd;
+
 pub fn main() {
     #[cfg(windows)]
     windows::main();
 
     #[cfg(target_os = "linux")]
     linux::main();
+    
+    #[cfg(target_os = "bsd")]
+    bsd::main();
 }
