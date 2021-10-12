bf93b3ee6dd9d2a42d218c54e898979ee7277010
vamolessa <1696072+vamolessa@users.noreply.github.com>
2021-10-12 19:58:17 -0300

    build and test alias commands

--- project.pp
+++ project.pp
@@ -1,5 +1,5 @@
 config indent_with_tabs false
 
-alias build [[enqueue-keys ":o build<enter>aad!cmd /c cargo build 2<greater>&1<enter>"]]
-alias test [[enqueue-keys ":o build<enter>aad!cmd /c cargo test 2<greater>&1<enter>"]]
+alias build [[enqueue-keys ":o build scratch<enter>aad!cmd /c cargo build 2<greater>&1<enter>"]]
+alias test [[enqueue-keys ":o test scratch<enter>aad!cmd /c cargo test 2<greater>&1<enter>"]]
 
