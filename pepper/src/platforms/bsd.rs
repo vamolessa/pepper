0901a669e06ce70237c4ad5c216a93bdce8849ee
vamolessa <1696072+vamolessa@users.noreply.github.com>
2021-11-16 15:09:50 -0300

    fix buffer len

--- pepper/src/platforms/bsd.rs
+++ pepper/src/platforms/bsd.rs
@@ -452,7 +452,7 @@ fn run_client(args: Args, mut connection: UnixStream) {
                 }
 
                 if libc::FD_ISSET(terminal.as_raw_fd(), &select_read_set) {
-                    buf.resize(data as _, 0);
+                    buf.resize(ClientApplication::stdin_buffer_len(), 0);
                     match read(terminal.as_raw_fd(), &mut buf) {
                         Ok(0) | Err(()) => break,
                         Ok(len) => terminal.parse_keys(&buf[..len], &mut keys),
