06b46666b6319e79b65010bb68d5edbca52186e7
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2021-11-17 23:07:33 -0300

    checkpoint

--- web/index.js
+++ web/index.js
@@ -1,11 +1,33 @@
-import init, {greet} from "./pkg/pepper_web.js";
+import init, {new_application} from "./pkg/pepper_web.js";
+
+const STATE = {
+    terminalElement: null,
+    application: null,
+};
 
 init().then(() => {
-    greet("WebAssembly")
+    STATE.application = new_application("helloer");
+    if (STATE.terminalElement != null) {
+        main();
+    }
 });
 
 window.onload = function() {
-    var term = new Terminal();
-    term.open(document.getElementById("terminal"));
+    STATE.terminalElement = document.getElementById("terminal");
+    if (STATE.application != null) {
+        main();
+    }
+}
+
+function main() {
+    say_hello(STATE.application);
+    var term = new Terminal({
+        cols: 130,
+        rows: 50,
+    });
+    term.open(STATE.terminalElement);
+    term.onKey(function(key) {
+        console.log(key);
+    });
     term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $");
 }
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,17 +1,28 @@
-mod web_platform;
-
 use wasm_bindgen::prelude::*;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     pub fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub struct Application {
+    terminal_size: (u16, u16),
 }
+
+#[wasm_bindgen]
+pub fn new_pepper_editor(terminal_width: u16, terminal_height: u16) -> *mut Application {
+    let app = Application {
+        terminal_size: (terminal_width, terminal_height),
+    };
+    Box::into_raw(Box::new(app))
+}
+
+#[wasm_bindgen]
+pub fn on_key(app: *mut Application, key: &str) {
+    //&[]
+}
+
 
